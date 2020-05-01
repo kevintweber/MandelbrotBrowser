f4f4058a40b3dea158148c0030650b94a3ad79cb
Kevin Weber <kevin.weber@site-kitchen.com>
2020-05-01 09:20:23 -0400

    Split out color function

--- src/calculation/Mandelbrot.ts
+++ src/calculation/Mandelbrot.ts
@@ -1,4 +1,5 @@
 import { Position } from "./Position";
+import hslToRgb from "../display/HslToRgb";
 
 /**
  * The primary class for Mandelbrot set calculations.
@@ -11,7 +12,6 @@ export class Mandelbrot {
     readonly centerY: number;
     readonly width: number;
     readonly maxIterations: number;
-    readonly escapeSquared: number;
     readonly increment: number;
 
     constructor(
@@ -62,7 +62,7 @@ export class Mandelbrot {
             return [ 0, 0, 0 ];
         }
 
-        return this.hslToRgb(((depth + 140) % 255) / 255, 1, 0.5);
+        return hslToRgb(((depth + 140) % 255) / 255, 1, 0.5);
     }
 
     determinePosition(x: number, y: number): Position {
@@ -84,31 +84,5 @@ export class Mandelbrot {
             img.data[offset++] = color[2];
             img.data[offset++] = 255;
         }
-    }
-
-    private hslToRgb(h, s, l) {
-        let r, g, b;
-
-        if (s == 0) {
-            r = g = b = l; // achromatic
-        } else {
-            var hue2rgb = function hue2rgb(p, q, t) {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-
-                return p;
-            }
-
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1 / 3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
-        }
-
-        return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
     }
 }
