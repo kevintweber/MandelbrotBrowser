6db8603ebd5a0646ba1d737257918a93e135a30b
Kevin Weber <kevin.weber@site-kitchen.com>
2020-10-16 12:43:36 -0400

    Max iterations for Julia images is now at least 2x more.

--- src/calculation/MaxIterations.ts
+++ src/calculation/MaxIterations.ts
@@ -2,12 +2,12 @@ import { ParameterHandler } from "../display/ParameterHandler";
 import { SetType } from "./engine/SetType";
 
 export default function determineMaxIterations(parameterHandler: ParameterHandler): number {
-    if (parameterHandler.set === SetType.Julia) {
-        return 2000;
-    }
-
     let logWidth = Math.log(5.0 / parameterHandler.width);
     let iterations = Math.floor(logWidth * logWidth * 30);
+
+    if (parameterHandler.set === SetType.Julia) {
+        return Math.max(iterations, 4000);
+    }
 
     return Math.max(iterations, 150);
 }
